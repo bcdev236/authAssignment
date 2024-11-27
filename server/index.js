031858a887077725f00152164ae5026bf7aae900
bcdev236 <bcdev236@gmail.com>
2024-11-27 17:18:19 +0530

    session-secure turned to true

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(
       saveUninitialized: false, 
       cookie: {
           maxAge: 24 * 60 * 60 * 1000, // 1 day
-          secure: false,
+          secure: true,
           httpOnly: true,
       },
   })
